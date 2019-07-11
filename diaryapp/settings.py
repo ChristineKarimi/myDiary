8a93095eb1ac0828ae6af839e114dc37716f3c65
christineKarimi <karimikim3@gmail.com>
2019-07-11 11:33:54 +0300

    Added static configurations to render static files in the settings fileee

--- diaryapp/settings.py
+++ diaryapp/settings.py
@@ -120,4 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,"static")
+]
+
+LOGIN_REDIRECT_URL ='/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
