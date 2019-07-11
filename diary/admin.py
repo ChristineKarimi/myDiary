611dda4c75e1fb2a89ec24157cab2489a211be35
christineKarimi <karimikim3@gmail.com>
2019-07-11 10:43:14 +0300

    Setting up of the main projects urls and django registration set up in the admin.py

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Neighborhood,NeighborProfile,Business
 
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(NeighborProfile)
+admin.site.register(Business)
