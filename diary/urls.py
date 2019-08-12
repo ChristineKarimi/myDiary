c8008801a09fd37cf5afd9fc005689fb540b1a12
christineKarimi <karimikim3@gmail.com>
2019-08-12 15:27:36 +0300

    Code refactor

--- diary/urls.py
+++ diary/urls.py
@@ -6,10 +6,10 @@ from django.conf.urls.static import static
 urlpatterns=[
   url(r'^$',views.home,name='home'),
   url(r'^accounts/',include('registration.backends.simple.urls')),
-  url(r'^post-details/(?P<neighborhood_id>[0-9])$',views.hoods,name='post-details'),
-  url(r'^new/newPost$',views.new_hood,name='newPost'),
-  url(r'^new/neighbour$',views.new_neighbour,name='newDiaryEntry'),
-  url(r'^new/newDiaryPost$',views.new_business,name='newDiaryPost'),
+  url(r'^hood/(?P<neighborhood_id>[0-9])$',views.hoods,name='hood-details'),
+  url(r'^new/neighborhood$',views.new_hood,name='newHood'),
+  url(r'^new/neighbour$',views.new_neighbour,name='newNeighbour'),
+  url(r'^new/business$',views.new_business,name='newBusiness'),
   url(r'^profile/',views.user_profile,name='userProfile'),
   url(r'^search/',views.search_business,name='search'),
 ]
